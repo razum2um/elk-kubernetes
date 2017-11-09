2b6c8ae0d129e7538a11114f66d857c7d4a1ddaf
Vlad Bokov <razum2um@mail.ru>
2017-11-09 20:23:17 +0700

    allow single persistent node

--- deploy.sh
+++ deploy.sh
@@ -24,11 +24,11 @@ eval "kubectl ${KUBECTL_PARAMS} create namespace \"${NAMESPACE}\""
 NODES=$(eval "${KUBECTL} get nodes -l 'kubernetes.io/role!=master' -o go-template=\"{{range .items}}{{\\\$name := .metadata.name}}{{\\\$unschedulable := .spec.unschedulable}}{{range .status.conditions}}{{if eq .reason \\\"KubeletReady\\\"}}{{if eq .status \\\"True\\\"}}{{if not \\\$unschedulable}}{{\\\$name}}{{\\\"\\\\n\\\"}}{{end}}{{end}}{{end}}{{end}}{{end}}\"")
 ES_DATA_REPLICAS=$(echo "$NODES" | wc -l)
 
-if [ "$ES_DATA_REPLICAS" -lt 3 ]; then
-  print_red "Minimum amount of Elasticsearch data nodes is 3 (in case when you have 1 replica shard), you have ${ES_DATA_REPLICAS} worker nodes"
-  print_red "Won't deploy more than one Elasticsearch data pod per node exiting..."
-  exit 1
-fi
+#if [ "$ES_DATA_REPLICAS" -lt 3 ]; then
+#  print_red "Minimum amount of Elasticsearch data nodes is 3 (in case when you have 1 replica shard), you have ${ES_DATA_REPLICAS} worker nodes"
+#  print_red "Won't deploy more than one Elasticsearch data pod per node exiting..."
+#  exit 1
+#fi
 
 print_green "Labeling nodes which will serve Elasticsearch data pods"
 for node in $NODES; do
