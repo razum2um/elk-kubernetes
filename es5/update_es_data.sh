c13473ccac8040e9cbeab8679236d673d98bb73e
Vlad Bokov <razum2um@mail.ru>
2017-11-10 00:11:51 +0700

    do 2.4 single

--- es5/update_es_data.sh
+++ es5/update_es_data.sh
@@ -22,11 +22,11 @@ KUBECTL="kubectl ${KUBECTL_PARAMS} --namespace=\"${NAMESPACE}\""
 NODES=$(eval "${KUBECTL} get nodes -o go-template=\"{{range .items}}{{\\\$name := .metadata.name}}{{\\\$unschedulable := .spec.unschedulable}}{{range .status.conditions}}{{if eq .reason \\\"KubeletReady\\\"}}{{if eq .status \\\"True\\\"}}{{if not \\\$unschedulable}}{{\\\$name}}{{\\\"\\\\n\\\"}}{{end}}{{end}}{{end}}{{end}}{{end}}\"")
 ES_DATA_REPLICAS=$(echo "$NODES" | wc -l)
 
-if [ "$ES_DATA_REPLICAS" -lt 3 ]; then
-  print_red "Minimum amount of Elasticsearch data nodes is 3 (in case when you have 1 replica shard), you have ${ES_DATA_REPLICAS} worker nodes"
-  print_red "Won't deploy more than one Elasticsearch data pod per node exiting..."
-  exit 1
-fi
+#if [ "$ES_DATA_REPLICAS" -lt 3 ]; then
+#  print_red "Minimum amount of Elasticsearch data nodes is 3 (in case when you have 1 replica shard), you have ${ES_DATA_REPLICAS} worker nodes"
+#  print_red "Won't deploy more than one Elasticsearch data pod per node exiting..."
+#  exit 1
+#fi
 
 render_template es-data.yaml.tmpl | eval "${KUBECTL} replace -f -"
 
